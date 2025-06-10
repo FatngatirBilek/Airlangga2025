fe8c96767cfe2fb1ea7836adcfd91b94dc41550a
FatngatirBilek <fathirbimashabri@gmail.com>
2025-06-10 13:12:43 +0700

    non user can't access editSuaraAll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider, SignedIn, UserButton } from "@clerk/nextjs";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,12 +24,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="mt-8 max-w-3xl mx-auto">{children}</div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <div className="mt-8 max-w-3xl mx-auto">{children}</div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
